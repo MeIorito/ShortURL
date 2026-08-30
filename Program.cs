0063bca1495a066b605f857e9e76f2f712120dd7
MeIorito <90834076+MeIorito@users.noreply.github.com>
2026-08-30 20:05:29 +0200

    Added mongodb connection requirements

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ShortURL.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp
+        .GetRequiredService<IOptions<MongoDbSettings>>()
+        .Value;
+
+    return new MongoClient(settings.ConnectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
