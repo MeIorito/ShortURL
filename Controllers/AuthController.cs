e12a76d272c65c174efcccd886a8aa6428cbb12e
MeIorito <90834076+MeIorito@users.noreply.github.com>
2026-08-30 20:27:52 +0200

    Auth controller and auth dtos added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,20 @@
 namespace ShortURL.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using ShortURL.DTOs.Auth;
 
 [ApiController]
 [Route("api/v1/auth")]
 public class AuthController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult Login(LoginDto login)
+    {
+        return Ok();
+    }
 
+    [HttpPost]
+    public IActionResult Register(RegisterDto register)
+    {
+        return Ok();
+    }
 }
