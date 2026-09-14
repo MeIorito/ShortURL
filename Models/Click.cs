8ced7ccfc29fcf52d24089839d0841f103acb9e0
MeIorito <90834076+MeIorito@users.noreply.github.com>
2026-09-14 13:08:17 +0200

    Click model updated

--- Models/Click.cs
+++ Models/Click.cs
@@ -2,20 +2,31 @@ namespace ShortURL.Models;
 
 public class Click
 {
-   private Click() {} 
+    private Click() { }
 
-   public Click(Guid urlId, DateTime timeStamp, string ipAddress)
+    public Click(
+        Guid urlId,
+        DateTime timeStamp,
+        string ipAddress,
+        string? userAgent,
+        string? referer)
     {
         UrlId = urlId;
         TimeStamp = timeStamp;
         IpAddress = ipAddress;
+        UserAgent = userAgent;
+        Referer = referer;
     }
 
-   public Guid Id { get; private set;}
-   
-   public Guid UrlId { get; private set;}
+    public Guid Id { get; private set; }
 
-   public DateTime TimeStamp { get; set;}
+    public Guid UrlId { get; private set; }
 
-   public required string IpAddress { get; set;}
+    public DateTime TimeStamp { get; private set; }
+
+    public string IpAddress { get; private set; } = string.Empty;
+
+    public string? UserAgent { get; private set; }
+
+    public string? Referer { get; private set; }
 }
